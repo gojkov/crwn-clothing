7caa839a4c922c4e8633c639c502641e48adc697
Steve Gojkov <stevegojkov@gmail.com>
2020-04-02 09:00:59 -0400

    added media queries for App.css & Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import { GlobalStyle } from './global.styles';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
@@ -47,6 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
